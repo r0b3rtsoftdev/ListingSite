a862e82e940d68e90fd076f09f0ca1cf0daac010
r0b3rtsoftdev <r0b3rtsoftdev@gmail.com>
2024-10-19 20:34:13 +0200

    Update format: double quote to single quote

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,39 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div class="container">
+    <button class="btn btn-primary">Bootstrap Button</button>
+  </div>
+
+  <ul
+    class="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+    id="pillNav2"
+    role="tablist"
+    style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white)"
+  >
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active rounded-5" id="home-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">Home</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link rounded-5" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Profile</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link rounded-5" id="contact-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Contact</button>
+    </li>
+  </ul>
+
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+    <ul class="dropdown-menu">
+      <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+      <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+      <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+    </ul>
+  </div>
+
+  ikon:
+  <i class="bi bi-alarm"></i>
+
+  <i class="bi bi-star"></i>
+  <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue"></i>
 </template>
