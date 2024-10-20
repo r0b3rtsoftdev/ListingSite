ef85d4f324392e6b8f8937803063940f37bae672
r0b3rtsoftdev <r0b3rtsoftdev@gmail.com>
2024-10-20 14:08:14 +0200

    Add List page

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,8 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: 'about' */ '../views/AboutView.vue'),
+    component: () => import(/* webpackChunkName: 'about' */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/list/:categoryName?',
+    name: 'list',
+    component: () => import(/* webpackChunkName: 'list' */ '../views/pages/ListPage.vue'),
   },
 ];
 
---
+++ src/views/pages/ListPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container d-flex">
+    <SidebarView />
+
+    <div class="flex-grow-1">Lista jön ide</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import SidebarView from '@/views/layout/SidebarView.vue';
+
+const route = useRoute();
+const { categoryName } = ref(route.params).value;
+
+onMounted(() => {
+  if (categoryName) {
+    console.log('Category Name:', categoryName);
+  } else {
+    console.log('No category specified.');
+  }
+});
+</script>
